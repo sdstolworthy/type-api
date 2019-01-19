9d11ae84159138729ca4d7ee5488f55926c1df52
Jacob Dick <jdick@altamir.io>
2019-01-18 20:27:12 -0700

    add logging for auth debug

--- src/api/data/index.ts
+++ src/api/data/index.ts
@@ -1,12 +1,21 @@
 import { ApolloServer } from 'apollo-server-express'
 import { Application } from 'express'
+import * as util from 'util'
 import { port } from '../../app'
 import { logger } from '../../config/logger'
+import getDecodedJwt from '../middleware/getDecodedJwt'
 import { resolvers, typeDefs } from './schema'
 
 export async function StartGraphQL(app: Application) {
 
-  const server = new ApolloServer({ typeDefs, resolvers })
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      // logger.debug('auth:')
+      // logger.debug(util.inspect(req.headers.authorization, {showHidden: false, depth: null}))
+    },
+  })
 
   server.applyMiddleware({ app }) // app is from an existing express app
 
--- src/api/data/user/user.query.ts
+++ src/api/data/user/user.query.ts
@@ -1,4 +1,6 @@
 import { gql } from 'apollo-server-express'
+import * as util from 'util'
+import { logger } from '../../../config/logger'
 import { User } from './user.entity'
 
 const Query = gql`
@@ -21,6 +23,7 @@ export const queryResolvers = {
     },
 
     async users(obj, { take, skip }, context, info) {
+      logger.info(util.inspect(context, {showHidden: false, depth: null}))
       take = take || 10 // default query limit to 10
       if (take > 50) { take = 50 } // limit query to 50 max
       skip = skip || 0 // default to none skipped
