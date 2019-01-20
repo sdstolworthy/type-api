0cc48384812c0a2ed21838c39bbb6c26dfd1ebec
Jacob Dick <jdick@altamir.io>
2019-01-19 20:06:49 -0700

    clean up getUserFromAuthHeader

--- src/api/middleware/getUserFromAuthHeader.ts
+++ src/api/middleware/getUserFromAuthHeader.ts
@@ -1,13 +1,10 @@
 import * as jwt from 'jsonwebtoken'
 import { Brackets } from 'typeorm'
-import * as util from 'util'
 import { logger } from '../../config/logger'
 import { User } from '../data/user/user.entity'
 
-const AUTH_HEADER = 'authorization'
-
 /**
- * Returns a decoded JWT from an auth HTTP bearer token.
+ * Returns a user from an auth bearer token.
  * https://www.thepolyglotdeveloper.com/2018/07/protect-graphql-properties-jwt-nodejs-application/
  * @function
  * @param {String} bearerToken - The authorization bearer token in the format "Bearer <token>".
@@ -19,57 +16,32 @@ const getUserFromAuthHeader = async (bearerToken: string) => {
     return {}
   }
 
-  const decodedToken = jwt.verify(bearerTokenArray[1], process.env.SECRET_KEY) // synchronous
+  return await jwt.verify(bearerTokenArray[1], process.env.SECRET_KEY, async (err, decodedToken) => {
+    if (err) {
+      logger.error('getUserFromAuthHeader() jwt.verify()')
+      logger.error(err)
+      return {}
+    }
 
-  return await User.createQueryBuilder('user')
-  .select('user.id') // only select necessary fields
-  // .addSelect('user.lastPasswordReset')
-  .where('user.id = :id', { id: decodedToken.id })
-  .andWhere(new Brackets((qb) => {
-    // prevent user auth if lastPasswordReset is after the jwt's iat value
-    qb.where('user.lastPasswordReset < :iat', { iat: new Date(decodedToken.iat * 1000) })
-    .orWhere('user.lastPasswordReset IS NULL')
-  }))
-  .getOne()
-  .then((user) => {
-    if (!user) { return {} }
-    return user
+    return await User.createQueryBuilder('user')
+    .select('user.id') // only select necessary fields
+    .where('user.id = :id', { id: decodedToken.id })
+    .andWhere(new Brackets((qb) => {
+      // prevent user auth if lastPasswordReset is after the jwt's iat value
+      qb.where('user.lastPasswordReset < :iat', { iat: new Date(decodedToken.iat * 1000) })
+      .orWhere('user.lastPasswordReset IS NULL')
+    }))
+    .getOne()
+    .then((user) => {
+      if (!user) { return {} }
+      return user
+    })
+    .catch((error) => {
+      logger.error('getUserFromAuthHeader() User.createQueryBuilder()')
+      logger.error(error)
+      return {}
+    })
   })
-  .catch((error) => {
-    logger.error(error)
-    return {}
-  })
-
-  // jwt.verify(bearerTokenArray[1], process.env.SECRET_KEY, async (err, decodedToken) => {
-  //   if (err) {
-  //     logger.debug('getUserFromAuthHeader() - Invalid JWT')
-  //     return {}
-  //   }
-
-  //   // Also check for valid JWT against user.lastPasswordReset
-  //   await User.createQueryBuilder('user')
-  //   .select('id')
-  //   .addSelect('user.lastPasswordReset')
-  //   .where('user.id = :id', { id: decodedToken.id })
-  //   .andWhere(new Brackets((qb) => {
-  //     // prevent user auth if lastPasswordReset is after the jwt's iat value
-  //     qb.where('user.lastPasswordReset < :iat', { iat: new Date(decodedToken.iat * 1000) })
-  //     .orWhere('user.lastPasswordReset IS NULL')
-  //   }))
-  //   .getOne()
-  //   .then((user) => {
-  //     if (!user) { return }
-
-  //     logger.debug(`user.lastPasswordReset: ${user.lastPasswordReset}`)
-  //     logger.debug(`decodedToken.iat: ${new Date(decodedToken.iat * 1000)}`)
-
-  //     return user
-  //   })
-  //   .catch((error) => {
-  //     logger.error(error)
-  //     return {}
-  //   })
-  // })
 }
 
 export default getUserFromAuthHeader
