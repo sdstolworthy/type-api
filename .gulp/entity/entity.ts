a8f096f3f99b65043757d542a6278c099e6644f8
Jacob Dick <jdick@altamir.io>
2019-01-20 19:56:50 -0700

    update entity name

--- .gulp/entity/entity.ts
+++ .gulp/entity/entity.ts
@@ -1,7 +1,9 @@
 import { Column, Entity, Index, OneToMany } from 'typeorm'
 import { AltamirEntity } from '../_helpers/baseEntity'
 
-@Entity()
+@Entity({
+  name: '<%= entityName%>s',
+})
 export class <%= entityName[0].toUpperCase() + entityName.slice(1) %> extends AltamirEntity {
   @Column()
   public title: string
