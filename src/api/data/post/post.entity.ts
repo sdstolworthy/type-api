05d98f7ac714c2dc36d0c0cd2c7ad163ef2f06ed
Jacob Dick <jdick@altamir.io>
2019-01-21 20:19:18 -0700

    change post entity name

--- src/api/data/post/post.entity.ts
+++ src/api/data/post/post.entity.ts
@@ -1,7 +1,9 @@
 import { Column, Entity, Index, OneToMany } from 'typeorm'
 import { AltamirEntity } from '../_helpers/baseEntity'
 
-@Entity()
+@Entity({
+  name: 'posts',
+})
 export class Post extends AltamirEntity {
   @Column()
   public title: string
