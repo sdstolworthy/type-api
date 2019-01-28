1ffc1aeb3136367a31b5eaae95f15fb101be4222
Jacob Dick <jdick@altamir.io>
2019-01-27 21:34:13 -0700

    add auth test

--- src/api/auth/index.spec.ts
+++ src/api/auth/index.spec.ts
@@ -1,11 +1,15 @@
-/* tslint:disable no-unused-expression */
-import { expect } from 'chai'
+/* tslint:disable no-unused-expression no-var-requires */
+import * as chai from 'chai'
 import 'mocha'
-import { logger } from '../../config/logger'
+import settings from '../../config/settings'
 import Server from '../../server'
+
+chai.use(require('chai-http'))
+const expect = chai.expect
 
 describe('auth endpoint', function() {
   this.timeout(0)
+  const baseUrl = `http://127.0.0.1:${settings.port}/auth`
 
   describe('POST /auth/register', () => {
     const server = new Server()
@@ -18,9 +22,19 @@ describe('auth endpoint', function() {
       server.close(done)
     })
 
-    it('returns true', (done) => {
-      expect(true).to.be.true
-      done()
+    it('returns an error when email is invalid', (done) => {
+      chai.request(baseUrl)
+      .post('/register')
+      .type('form')
+      .send({
+        email: 'test@gmail', // invalid email
+        password: 'testPassword',
+      })
+      .end((err, res) => {
+        expect(err).to.be.null
+        expect(res).to.have.status(400)
+        done()
+      })
     })
   })
 })
