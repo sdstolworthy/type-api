d193f1bce75de4daf4bb8e188babad9e4363975e
Jacob Dick <jdick@altamir.io>
2019-01-29 21:18:45 -0700

    update docker-compose files

--- docker.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [string]$env = "development"
-)
-
-if ($env -eq "test") {
-  docker-compose.exe -f ./docker-compose.test.yml up
-} else {
-  docker-compose.exe -f ./docker-compose.development.yml up
-}
