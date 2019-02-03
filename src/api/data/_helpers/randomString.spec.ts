481ceac8d0a14dad6a4bf00a5eebbf27dbf6018a
Jacob Dick <jdick@altamir.io>
2019-02-03 13:07:31 -0700

    add test for randomString

---
+++ src/api/data/_helpers/randomString.spec.ts
@@ -0,0 +1,23 @@
+/* tslint:disable no-unused-expression */
+import { expect } from 'chai'
+import 'mocha'
+import randomString from './randomString'
+
+describe('randomString', () => {
+  it('should return a random string of length x when given x (int)', (done: () => void) => {
+    const length: number = 10
+    const result: string = randomString(length)
+
+    expect(result).to.be.a('string')
+    expect(result).to.have.lengthOf(length)
+    done()
+  })
+
+  it('should return a random string of length 10 when not given any length', (done: () => void) => {
+    const result: string = randomString()
+
+    expect(result).to.be.a('string')
+    expect(result).to.have.lengthOf(10)
+    done()
+  })
+})
--- src/api/data/_helpers/randomString.ts
+++ src/api/data/_helpers/randomString.ts
@@ -1,3 +1,13 @@
-export default (length: number = 6): string => {
-  return Math.random().toString(36).substring(length)
+import * as crypto from 'crypto'
+
+// https://stackoverflow.com/a/5191133/5623385
+function roundUp(num: number, precision: number) {
+  precision = Math.pow(10, precision)
+  return Math.ceil(num * precision) / precision
+}
+
+export default (length: number = 10): string => {
+  // generate random hex bytes using half of length since hex is 2 digits for every 1 base 10 digit
+  const half: number = roundUp(length / 2, 0) // may be more than half of length
+  return crypto.randomBytes(half).toString('hex').slice(-length) // ensure length
 }
