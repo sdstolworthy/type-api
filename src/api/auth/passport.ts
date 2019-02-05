bcb60666d21b5b318f298615df153de63816b4ac
Jacob Dick <jdick@altamir.io>
2019-02-05 05:56:53 -0700

    lose .then in favor of async

--- src/api/auth/passport.ts
+++ src/api/auth/passport.ts
@@ -12,52 +12,51 @@ passport.use(new Local.Strategy(
     usernameField: 'email',
     passwordField: 'password',
   },
-  (email: string, password: string, done: CallableFunction) => {
-    User.createQueryBuilder('user')
-      .addSelect('user.password')
-      .where('user.email = :email', { email })
-      .getOne()
-      .then((user: User) => {
-        if (!user) { return done(null, false) }
-        if (!validatePassword(password, user.password)) { return done(null, false) }
-        return done(null, user)
-      })
-      .catch((err: any) => {
-        logger.error(err)
-        return done(err, false)
-      })
+  async (email: string, password: string, done: CallableFunction) => {
+    try {
+      const user: User = await User.createQueryBuilder('user')
+        .addSelect('user.password')
+        .where('user.email = :email', { email })
+        .getOne()
+
+      if (!user) { return done(null, false) }
+      if (!validatePassword(password, user.password)) { return done(null, false) }
+      return done(null, user)
+    } catch (err) {
+      logger.error(err)
+      return done(err, false)
+    }
   },
 ))
 
-passport.use(new JWT.Strategy({
-  jwtFromRequest: JWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: settings.secretKey,
-}, async (data: any, done: CallableFunction) => {
-  try {
-    User.createQueryBuilder('user')
-      .addSelect('user.lastPasswordReset')
-      .where('user.id = :id', { id: data.id })
-      .andWhere(new Brackets((qb) => {
-        // prevent user auth if lastPasswordReset is after the jwt's iat value
-        qb.where('user.lastPasswordReset < :iat', { iat: new Date(data.iat * 1000) })
-        .orWhere('user.lastPasswordReset IS NULL')
-      }))
-      .getOne()
-      .then((user: User) => {
-        if (!user) { return done(null, false) }
+passport.use(new JWT.Strategy(
+  {
+    jwtFromRequest: JWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: settings.secretKey,
+  },
+  async (data: any, done: CallableFunction) => {
+    try {
+      const user: User = await User.createQueryBuilder('user')
+        .addSelect('user.lastPasswordReset')
+        .where('user.id = :id', { id: data.id })
+        .andWhere(new Brackets((qb) => {
+          // prevent user auth if lastPasswordReset is after the jwt's iat value
+          qb.where('user.lastPasswordReset < :iat', { iat: new Date(data.iat * 1000) })
+          .orWhere('user.lastPasswordReset IS NULL')
+        }))
+        .getOne()
 
-        logger.debug(`user.lastPasswordReset: ${user.lastPasswordReset}`)
-        logger.debug(`data.iat: ${new Date(data.iat * 1000)}`)
+      if (!user) { return done(null, false) }
 
-        return done(null, user)
-      })
-      .catch((err: any) => {
-        return done(err, false)
-      })
-  } catch (err) {
-    logger.error(err)
-    return done(err)
-  }
-}))
+      logger.debug(`user.lastPasswordReset: ${user.lastPasswordReset}`)
+      logger.debug(`data.iat: ${new Date(data.iat * 1000)}`)
+
+      return done(null, user)
+    } catch (err) {
+      logger.error(err)
+      return done(err)
+    }
+  },
+))
 
 export const isAuthenticated = passport.authenticate('jwt', { session: false, failWithError: true })
--- src/api/middleware/getUserFromAuthHeader.ts
+++ src/api/middleware/getUserFromAuthHeader.ts
@@ -11,40 +11,33 @@ import { User } from '../data/user/user.entity'
  * @param {String} bearerToken - The authorization bearer token in the format "Bearer <token>".
  */
 const getUserFromAuthHeader = async (bearerToken: string) => {
-  const bearerTokenArray = bearerToken.split(' ')
-  if (bearerTokenArray.length !== 2 || bearerTokenArray[0].toLowerCase() !== 'bearer') {
-    logger.debug('getUserFromAuthHeader() - Authorization bearer token was not formatted properly or did not exist.')
-    return {}
-  }
-
-  return await jwt.verify(bearerTokenArray[1], settings.secretKey, async (err, decodedToken: any) => {
-    if (err) {
-      logger.debug('getUserFromAuthHeader() jwt.verify()')
-      logger.debug(err)
+  try {
+    const bearerTokenArray = bearerToken.split(' ')
+    if (bearerTokenArray.length !== 2 || bearerTokenArray[0].toLowerCase() !== 'bearer') {
+      logger.debug('getUserFromAuthHeader() - Authorization bearer token was not formatted properly or did not exist.')
       return {}
     }
 
-    return await User.createQueryBuilder('user')
-    .select('user.id') // only select necessary fields
-    .where('user.id = :id', { id: decodedToken.id })
-    .andWhere(new Brackets((qb) => {
-      // Prevent user auth if lastPasswordReset is after the jwt's iat value.
-      // This allows password resets to happen without keeping track of JWTs,
-      // which is the whole point of JWTs.
-      qb.where('user.lastPasswordReset < :iat', { iat: new Date(decodedToken.iat * 1000) })
-      .orWhere('user.lastPasswordReset IS NULL')
-    }))
-    .getOne()
-    .then((user) => {
-      if (!user) { return {} }
-      return user
-    })
-    .catch((error) => {
-      logger.error('getUserFromAuthHeader() User.createQueryBuilder()')
-      logger.error(error)
-      return {}
-    })
-  })
+    const decodedToken: any = await jwt.verify(bearerTokenArray[1], settings.secretKey)
+
+    const user: User = await User.createQueryBuilder('user')
+      .select('user.id') // only select necessary fields
+      .where('user.id = :id', { id: decodedToken.id })
+      .andWhere(new Brackets((qb) => {
+        // Prevent user auth if lastPasswordReset is after the jwt's iat value.
+        // This allows password resets to happen without keeping track of JWTs,
+        // which is the whole point of JWTs.
+        qb.where('user.lastPasswordReset < :iat', { iat: new Date(decodedToken.iat * 1000) })
+        .orWhere('user.lastPasswordReset IS NULL')
+      }))
+      .getOne()
+
+    if (!user) { return {} }
+    return user
+  } catch (err) {
+    logger.error(err)
+    return {}
+  }
 }
 
 export default getUserFromAuthHeader
