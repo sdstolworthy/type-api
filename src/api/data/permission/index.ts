805f274ab0bd0db6c7e80bd51fb46d764934f299
Jacob Dick <jdick@altamir.io>
2019-02-07 12:14:56 -0700

    fix permissions

--- src/api/data/permission/index.ts
+++ src/api/data/permission/index.ts
@@ -1,10 +1,7 @@
-import inputTypes from './permission.args'
-import { mutationResolvers, mutationTypes } from './permission.mutations'
 import { queryResolvers, queryTypes } from './permission.queries'
-import { subscriptionResolvers, subscriptionTypes } from './permission.subscriptions'
 import { typeResolvers, types } from './permission.type'
 
 export default {
-  types: () => [types, queryTypes, inputTypes, subscriptionTypes],
-  resolvers: Object.assign(queryResolvers, subscriptionResolvers, typeResolvers),
+  types: () => [types, queryTypes],
+  resolvers: Object.assign(queryResolvers, typeResolvers),
 }
--- src/api/data/permission/permission.args.ts
+++
@@ -1,7 +0,0 @@
-/**
- * permission.args.ts
- * All of the arguments for Mutations.
- */
-const Input = ''
-
-export default () => [ Input ]
