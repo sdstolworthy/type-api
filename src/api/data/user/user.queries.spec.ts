7489880b0357c0d55adca86afb028ed401deab6b
Jacob Dick <jdick@altamir.io>
2019-02-16 15:19:53 -0700

    update query tests

--- src/api/data/user/user.queries.spec.ts
+++ src/api/data/user/user.queries.spec.ts
@@ -1,16 +1,14 @@
-/* tslint:disable no-unused-expression newline-per-chained-call */
+/* tslint:disable no-unused-expression */
 import { queryResolvers, queryTypes } from './user.queries'
 
 describe('user queries', () => {
-  it('should export queryTypes', (done: () => void) => {
+  it('should export queryTypes', () => {
     expect(typeof queryTypes).toBe('function')
     // expect(typeof queryTypes()).toBe('array')
-    done()
   })
 
-  it('should export queryResolvers', (done: () => void) => {
+  it('should export queryResolvers', () => {
     expect(typeof queryResolvers).toBe('object')
     expect(queryResolvers).toHaveProperty('Query')
-    done()
   })
 })
