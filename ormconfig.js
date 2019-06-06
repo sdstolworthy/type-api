d4abcc0c6f38fe29701a3c4200bdddf142c7d999
Jacob Dick <jdick@altamir.io>
2019-06-06 01:44:02 -0600

    update packages

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,9 @@
-const connectionOptions = require('./src/config/db').connectionOptions
-
 /**
  * ormconfig.js
  * Keep this file so we can easily interact with the typeorm cli.
  */
+const connectionOptions = require('./src/config/db').connectionOptions
+
 module.exports = {
   ...connectionOptions,
   cli:  {
